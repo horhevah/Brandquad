b0eff0beb669e8233a55483f3927f8851e3343e8
horhevah <horhevah@mail.ru>
2021-04-15 22:29:22 +0300

    First step

---
+++ logger/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import ApacheLog
+
+admin.site.register(ApacheLog)
+# Register your models here.
+
---
+++ logger/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class ApacheLog(models.Model):
+    ip_address = models.GenericIPAddressField(protocol='both', unpack_ipv4=True,verbose_name = 'ip адрес')
+    data_time = models.DateTimeField(verbose_name='Дата')
+    method = models.CharField(max_length=10,verbose_name='Метод')
+    url = models.URLField(verbose_name='URL')
+    status_code = models.IntegerField(verbose_name='Код ответа')
+    size = models.IntegerField(verbose_name='Размер')
+
+    class Meta:
+        verbose_name_plural = 'Записи лога'
+        verbose_name = 'Запись лога'
+
+# Create your models here.
