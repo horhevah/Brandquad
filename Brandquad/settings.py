d38ab56b12a6c93359e64cb9cf3394c6e5834661
horhevah <horhevah@mail.ru>
2021-04-18 20:35:05 +0300

    secret key

--- Brandquad/settings.py
+++ Brandquad/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .key import KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&d!cnyfjj+_lc^3zt0yw$pkh_)@0yvh$v$(w^zeg9$!-k6tq)z'
+SECRET_KEY = KEY
+
+# print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
